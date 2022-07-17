fe4d5caf73a2f8edcbf2489f57452f72c922f62b
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2022-07-17 17:48:37 +0200

    landing page

---
+++ modules/landingPage/components/Button/LandingPageButton.styles.ts
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+import {colors} from '../../../global/theme/colors';
+
+export const styles = StyleSheet.create({
+  landingPageButtonConnection: {
+    height: 50,
+    width: 260,
+    marginTop: 20,
+  },
+  landingPageButtonSubscription: {
+    height: 50,
+    width: 260,
+    backgroundColor: colors.textColors.textWhite,
+    marginTop: 20,
+  },
+  landingPageButtonDriver: {
+    height: 50,
+    width: 260,
+    marginTop: 20,
+  }
+  landingPageButtonTextWhite: {
+    color: colors.textColors.textWhite,
+  },
+  landingPageButtonTextPurple: {
+    color: colors.textColors.textPurple,
+  },
+});
